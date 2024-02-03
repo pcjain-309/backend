2e2937d75af61c0eb6270afca1bc34766f5b683a
Paramveer Singh Choudhary <paramveer20@iitk.ac.in>
2024-02-03 23:56:33 +0530

    Removed some settings

--- app.py
+++ app.py
@@ -23,8 +23,8 @@ app = Flask(__name__)
 CORS(app)
 
 
-port = int(os.environ.get('PORT', 5000))
-app.run(host='0.0.0.0', port=port)
+# port = int(os.environ.get('PORT', 5000))
+# app.run(host='0.0.0.0', port=port)
 
 UPLOAD_FOLDER = 'uploads'
 ALLOWED_EXTENSIONS = {'csv'}
